887869310e3a4865c139fe6426c03f79030b3a54
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-02 14:22:06 -0400

    style list

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import ListItem from './ListItem'
-import '../styles/List'
+import '../styles/List.css'
 
 const List = props => {
   let items = props.items.map((item, i) => {
     return <ListItem key={i} data={item} />
   })
-  console.log(items)
-  return <div>{items}</div>
+  return <div className="list">{items}</div>
 }
 
 export default List
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import '../styles/ListItem'
+import '../styles/ListItem.css'
 
 const ListItem = props => {
+  let status = props.data.completed
+    ? 'complete list-item'
+    : 'incomplete list-item'
   return (
-    <div className="list-item">
+    <div className={status}>
       <div>{props.data.name}</div>
       <div>{props.data.createdAt}</div>
-      <div>{props.data.completed}</div>
       <div>{props.data.id}</div>
-      <div>{props.data.likes}</div>
+      <div>{props.data.likes.length}</div>
       <div>{props.data.location}</div>
       <div>{props.data.addedBy}</div>
       <div>{props.data.link}</div>
       <div>{props.data.type}</div>
       <div>{props.data.private}</div>
-      <div>hello</div>
     </div>
   )
 }
