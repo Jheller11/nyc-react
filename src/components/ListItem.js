ec0e7cd27444546c3fe24e8d9f4e80150d49e0f8
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-02 16:21:24 -0400

    style list items and buttons

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,19 +5,44 @@ const ListItem = props => {
   let status = props.data.completed
     ? 'complete list-item'
     : 'incomplete list-item'
+  let d = new Date(props.data.createdAt)
   return (
     <div className={status}>
-      <div>{props.data.name}</div>
-      <div>{props.data.createdAt}</div>
-      <div>{props.data.id}</div>
-      <div>{props.data.likes.length}</div>
-      <div>{props.data.location}</div>
-      <div>{props.data.addedBy}</div>
-      <div>{props.data.link}</div>
-      <div>{props.data.type}</div>
-      <div>{props.data.private}</div>
+      <div className="top-row">
+        <div>
+          <a href={props.data.link} target="_blank">
+            {props.data.name}
+          </a>
+        </div>
+        <div>{props.data.addedBy}</div>
+        <div>{props.data.location}</div>
+      </div>
+      <div className="buttons">
+        <button>
+          <i className="fa fa-heart-o fa-lg" /> {props.data.likes.length}
+        </button>
+        <button>
+          <i className="fa fa-map-marker fa-lg" />
+        </button>
+        <button>
+          <i className="fa fa-pencil-square-o fa-lg" />
+        </button>
+        <button>
+          <i className="fa fa-trash-o fa-lg" />
+        </button>
+      </div>
     </div>
   )
 }
 
 export default ListItem
+
+/* <div>{props.data.name}</div>
+      <div>{props.data.createdAt}</div>
+      <div>{props.data._id}</div>
+      <div>{props.data.likes.length}</div>
+      <div>{props.data.location}</div>
+      <div>{props.data.addedBy}</div>
+      <div>{props.data.link}</div>
+      <div>{props.data.type}</div>
+      <div>{props.data.private ? 'private' : 'public'}</div> */
