63e9b974be72919f39926ed9aba9938aeed6dc30
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-02 14:35:18 -0400

    add filter

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ListItem from './ListItem'
 import '../styles/List.css'
 
-const List = props => {
-  let items = props.items.map((item, i) => {
-    return <ListItem key={i} data={item} />
-  })
-  return <div className="list">{items}</div>
+class List extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      view: 'all'
+    }
+  }
+
+  render() {
+    let list = () => {
+      switch (this.state.view) {
+        case 'all':
+          let items = this.props.items.map((item, i) => {
+            return <ListItem key={i} data={item} />
+          })
+          return items
+        case 'complete':
+          items = this.props.items
+            .filter(item => {
+              return item.completed
+            })
+            .map((item, i) => {
+              return <ListItem key={i} data={item} />
+            })
+          return items
+        case 'incomplete':
+          items = this.props.items
+            .filter(item => {
+              return !item.completed
+            })
+            .map((item, i) => {
+              return <ListItem key={i} data={item} />
+            })
+          return items
+        default:
+          break
+      }
+    }
+
+    return <div className="list">{list()}</div>
+  }
 }
 
 export default List
