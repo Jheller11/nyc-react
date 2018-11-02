e36dea13f1f7caa39f8a03f01691b3b61bbbc105
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-02 12:12:15 -0400

    add style folder/imports

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ListItem from './ListItem'
+import '../styles/List'
 
 const List = props => {
   let items = props.items.map((item, i) => {
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/ListItem'
 
 const ListItem = props => {
   return (
