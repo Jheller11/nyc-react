2cb6921dd3cb0feaf653cbbda118ac3be261f98a
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-04 19:22:22 -0500

    input component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Login extends Component {
   constructor() {
     super()
     this.state = {}
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    let target = e.target.name
+    let value = e.target.value
+    this.setState({
+      [target]: value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    axios
+      .post('/users/login', this.state)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   render() {
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Signup extends Component {
   constructor() {
     super()
     this.state = {}
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    let target = e.target.name
+    let value = e.target.value
+    this.setState({
+      [target]: value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    axios
+      .post('/users/signup', this.state)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   render() {
